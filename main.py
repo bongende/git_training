0b1f55530e1321a99f3b9896ef68842ec300a230
ChristoBongende <100066566+bongende@users.noreply.github.com>
2022-07-04 23:18:30 +0200

    Update main.py
    I wanted to make it more engage for a script
    especialy by adding another function

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 def createFunction():
     print("it not as dificult i it can look like ")
 
+name= input('please inmut your name')
+def create_some_idea(name):
+    print('this function was writen just for test some comit on git hub')
+    print("hello" + name + 'thank you for doing the test')
+    
